6472409603b1fcc60999d621b903fc587bd3140e
Kacper Mazur <kacper.mazur888@gmail.com>
2019-01-12 02:14:34 +0000

    details constructor

--- PatientManagementSystem/src/usermodel/Details.java
+++ PatientManagementSystem/src/usermodel/Details.java
@@ -5,10 +5,56 @@
  */
 package usermodel;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author kacper
  */
 public class Details {
+    private String name;
+    private String surname;
+    private LocalDate birthDate;
+    private String password;
+
+    public Details(String name, String surname, LocalDate birthDate, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     
 }
