f38cef9148fb02fbf8807f6e16d92e42216b68f9
Kacper Mazur <kacper.mazur888@gmail.com>
2019-01-13 14:02:20 +0000

    Medice constructor and gettter/setters

--- PatientManagementSystem/src/usermodel/Medicine.java
+++ PatientManagementSystem/src/usermodel/Medicine.java
@@ -10,5 +10,38 @@ package usermodel;
  * @author kacper
  */
 public class Medicine {
+    private String name;
+    private Integer qunatity;
+
+    public Medicine(String name, Integer qunatity) {
+        this.name = name;
+        this.qunatity = qunatity;
+    }
+
+    public String getName() {
+        if (name != null)
+        {
+            return name;
+        }
+        
+        return "no name Set";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQunatity() {
+        if (qunatity != null)
+        {
+            return qunatity;
+        }
+        return 0;
+    }
+
+    public void setQunatity(Integer qunatity) {
+        this.qunatity = qunatity;
+    }
+    
     
 }
