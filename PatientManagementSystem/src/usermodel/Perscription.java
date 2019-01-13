40acbd4bd405a4576e610311f0c0f21e16fb1138
Kacper Mazur <kacper.mazur888@gmail.com>
2019-01-13 14:07:20 +0000

    perscription

--- PatientManagementSystem/src/usermodel/Perscription.java
+++ PatientManagementSystem/src/usermodel/Perscription.java
@@ -10,5 +10,49 @@ package usermodel;
  * @author kacper
  */
 public class Perscription {
+    private Medicine perscribedMedicine;
+    private String dosage;
+    private String notes;
+
+    public Perscription(Medicine perscribedMedicine, String dosage) {
+        this.perscribedMedicine = perscribedMedicine;
+        this.dosage = dosage;
+    }
+
+    public Perscription(Medicine perscribedMedicine, String dosage, String notes) {
+        this.perscribedMedicine = perscribedMedicine;
+        this.dosage = dosage;
+        this.notes = notes;
+    }
+
+    public Medicine getPerscribedMedicine() {
+        return perscribedMedicine;
+    }
+
+    public void setPerscribedMedicine(Medicine perscribedMedicine) {
+        this.perscribedMedicine = perscribedMedicine;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getNotes() {
+        if(notes != null)
+        {
+            return notes;
+        }
+        
+        return "no notes recored";
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+    
     
 }
